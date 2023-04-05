d6fa124b7dd23dd0fea5a3a9f913802044d903f6
ChaoticIssam <issamzitouni257@gmail.com>
2023-04-05 17:13:58 +0000

    top

---
+++ flood_fill/flood_fill.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+void	flood_fill(char **str)
+{
+	int	i = 0;
+	if (str[i] == '1')
+		return ;
+	
+}
---
+++ ft_rrange/ft_rrange.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+int		*ft_rrange(int start, int end)
+{
+	int		*rrange;
+	int		i;
+
+	if (start > end)
+		rrange = malloc(sizeof(int) * (start - end) + 1);
+	else
+		rrange = malloc(sizeof(int) * (end - start) + 1);
+	i = 0;
+	while (start != end)
+	{
+		rrange[i++] = end;
+		end -= (start > end) ? -1 : 1;
+	}
+	rrange[i] = end;
+	return (rrange);
+}
